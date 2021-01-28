af3d85116ac8e5454bbe60f8a8e94106b1351507
darth-xiaozhijun <15717061273@163.com>
2021-01-28 23:13:07 +0800

    缓存穿透

--- server/src/main/java/com/geekshow/server/MainApplication.java
+++ server/src/main/java/com/geekshow/server/MainApplication.java
@@ -1,11 +1,13 @@
 package com.geekshow.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.geekshow.middleware.model")
 public class MainApplication extends SpringBootServletInitializer {
 
     @Override
